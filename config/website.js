5809fc264fe554496c6f3682b2e51b3a411f7a6f
Derek Fong <derek-fong@users.noreply.github.com>
2018-12-22 22:38:37 +0800

    feat: customise page (#3)

--- config/website.js
+++ config/website.js
@@ -1,21 +1,14 @@
-const tailwind = require('../tailwind');
-
 module.exports = {
   pathPrefix: '/SilverCubes', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'SilverCubes', // Navigation and Site Title
+  siteTitleAlt: 'SilverCubes', // Alternative Site title for SEO
+  siteUrl: 'http://www.alppix.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
+  siteLogo: '/logos/logo-512x512.png', // Used for SEO and manifest
+  siteDescription: 'Alppix home page. ',
 
   // Manifest and Progress color
-  themeColor: tailwind.colors.orange,
-  backgroundColor: tailwind.colors.blue,
+  themeColor: '#002b36',
+  backgroundColor: '#2aa198',
 };
---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+  window.location = '/';
+
+  return null;
+};
+
+export default NotFound;
