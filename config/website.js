c77a2f0bd2e56a3a634707f43808a8a5ff98fe8a
Derek Fong <derek.fong@alppix.com>
2018-12-22 22:37:16 +0800

    feat: update icons

--- config/website.js
+++ config/website.js
@@ -1,5 +1,3 @@
-const tailwind = require('../tailwind');
-
 module.exports = {
   pathPrefix: '/SilverCubes', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
@@ -7,10 +5,10 @@ module.exports = {
   siteTitleAlt: 'SilverCubes', // Alternative Site title for SEO
   siteUrl: 'http://www.alppix.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteLogo: '/logos/logo-512x512.png', // Used for SEO and manifest
   siteDescription: 'Alppix home page. ',
 
   // Manifest and Progress color
-  themeColor: tailwind.colors.orange,
-  backgroundColor: tailwind.colors.blue,
+  themeColor: '#002b36',
+  backgroundColor: '#2aa198',
 };
